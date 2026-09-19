2e065593173e1f4e07ba4cdcdf436de84c302661
zineb.hssini03891 <zainab.malak092@gmail.com>
2026-09-19 15:13:13 +0200

    Add challenge 1

---
+++ 1-Calculatrice de Base.js
@@ -0,0 +1,14 @@
+const prompt = require("prompt-sync")();
+let n1 = Number(prompt("Entrer le premier nombre: "));
+let n2 = Number(prompt("Entrer le deusième nombre: ")); 
+
+let somme = n1 + n2;
+let difference = n1 - n2;
+let produit = n1 * n2;
+let quotient = n1 / n2;
+
+console.log(`La somme est: ${somme}`);
+console.log(`La difference est: ${difference}`);
+console.log(`La produit est: ${produit}`);
+console.log(`La quotient est: ${quotient}`);
+
